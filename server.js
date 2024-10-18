f1574b19bb6907293379f6a0b9fa0abed9abe873
Squidisverycool <konun893@gmail.com>
2024-10-18 13:37:37 +0700

    implementing socket

--- server.js
+++ server.js
@@ -5,21 +5,30 @@ const io = require('socket.io')(http);
 
 const PORT = process.env.PORT || 3000;
 
+// Serve the index.html file from the root route
 app.get('/', (req, res) => {
-    res.send('Escape Plan Server is Running');
+    res.sendFile(__dirname + '/public/index.html'); // Adjust this line to serve your HTML file
 });
+
+// Serve static files (like CSS, JS) from the public directory
+app.use(express.static('public'));
 
 http.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
 
+// Handle Socket.IO connections
 io.on('connection', (socket) => {
     console.log('A player connected', socket.id);
-  
+
+    socket.on('error', (err) => {
+        console.error('Socket error:', err); // Log socket errors
+    });
+
     socket.on('disconnect', () => {
         console.log('Player disconnected', socket.id);
     });
-  
+
     socket.on('move', (data) => {
         console.log('Move received:', data);
         io.emit('update_state', data); // Broadcast the move to all players
