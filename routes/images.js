3f0271debd2fec0ac2e8463d032a0a0c02b61ade
PeakPattanun <peaksg31@gmail.com>
2024-11-07 21:38:35 +0700

    Remove unnecessary files

--- routes/images.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { getThiefImage } = require('../controllers/imageController');
-const router = express.Router();
-
-router.get('/image', getThiefImage);
-
-module.exports = router;
